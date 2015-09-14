08c14a568097b6d0eed8ca5187808fd6394b0b8a
Nacho L <nacho@nlopez.io>
2015-09-14 20:59:40 +0100

    Added multimapper example (not working yet)

---
+++ sample/src/main/java/io/nlopez/smartadapters/sample/MultiRecyclerViewCustomBuilderActivity.java
@@ -0,0 +1,45 @@
+package io.nlopez.smartadapters.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import io.nlopez.smartadapters.SmartAdapter;
+import io.nlopez.smartadapters.sample.model.Place;
+import io.nlopez.smartadapters.sample.model.User;
+import io.nlopez.smartadapters.sample.util.DataGenerator;
+import io.nlopez.smartadapters.sample.view.PlaceView;
+import io.nlopez.smartadapters.sample.view.UserAltView;
+import io.nlopez.smartadapters.sample.view.UserView;
+
+public class MultiRecyclerViewCustomBuilderActivity extends Activity {
+
+    @InjectView(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_recyclerview);
+
+        ButterKnife.inject(this);
+        initView();
+    }
+
+    private void initView() {
+        List mixedList = DataGenerator.generateMix(100);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        SmartAdapter.items(mixedList)
+                .map(User.class, UserView.class)
+                .map(User.class, UserAltView.class)
+                .map(Place.class, PlaceView.class)
+                .into(recyclerView);
+    }
+
+
+}
