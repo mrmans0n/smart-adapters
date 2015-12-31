625e6e23342cc5b146a15755e6644aedbc02b25b
筱龙缘 <794334212@qq.com>
2015-12-31 20:39:49 +0800

    Restored here to change
    Signed-off-by: xiaolongyuan <794334212@qq.com>

--- sample/src/main/java/io/nlopez/smartadapters/sample/SingleRecyclerViewActivity.java
+++ sample/src/main/java/io/nlopez/smartadapters/sample/SingleRecyclerViewActivity.java
@@ -2,20 +2,18 @@ package io.nlopez.smartadapters.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import io.nlopez.smartadapters.SmartAdapter;
 import io.nlopez.smartadapters.adapters.RecyclerMultiAdapter;
-import io.nlopez.smartadapters.builders.DefaultBindableLayoutBuilder;
 import io.nlopez.smartadapters.sample.model.User;
 import io.nlopez.smartadapters.sample.util.DataGenerator;
 import io.nlopez.smartadapters.sample.view.UserView;
-import io.nlopez.smartadapters.utils.Mapper;
-
-import java.util.List;
 
 public class SingleRecyclerViewActivity extends Activity {
 
@@ -34,21 +32,7 @@ public class SingleRecyclerViewActivity extends Activity {
     private void initView() {
         List<User> userList = DataGenerator.generateUsers(100);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        RecyclerMultiAdapter adapter = SmartAdapter.items(userList).map(User.class, UserView.class)
-                .builder(new DefaultBindableLayoutBuilder() {
-                    @Override
-                    public long viewItemId(@NonNull Object item, int position, @NonNull Mapper mapper) {
-                        System.out.println(String.format("position: %d,item:%s",position,item.toString()));
-                        return super.viewItemId(item, position, mapper);
-                    }
-
-                    @Override
-                    public boolean hasStableIds() {
-                        return true;
-                    }
-                }).into(recyclerView);
-
-
+        RecyclerMultiAdapter adapter = SmartAdapter.items(userList).map(User.class, UserView.class).into(recyclerView);
 
     }
 
